f7cbf3fb0cb14b26bbd233b833120074ef056ac8
collins13 <rashidcollins16@gmail.com>
2022-10-13 06:59:20 +0300

    changes

---
+++ src/components/Modal.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="backdrop">
+        <div class="modal">
+            <p>Modal Content</p>
+        </div>
+    </div>
+</template>
+
+
+<style>
+    .modal{
+        width: 400px;
+        padding: 10px;
+        margin: 100px;
+        background-color: white;
+        border-radius:10px;
+    }
+    .backdrop{
+        top: 0px;
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+</style>
